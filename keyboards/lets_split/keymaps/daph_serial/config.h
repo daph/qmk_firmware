30b5e9ae23254afa2eb721a2f071c3f94f69e13d
David Phillips <david@daviddouglasphillips.com>
2017-01-28 15:29:29 -0800

    LS: Setup for RGB

--- keyboards/lets_split/keymaps/daph_serial/config.h
+++ keyboards/lets_split/keymaps/daph_serial/config.h
@@ -15,15 +15,17 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "../../rev2/config.h"
 
 #define USE_SERIAL
 
 #define EE_HANDS
 
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
 
-#ifdef SUBPROJECT_rev1
-    #include "../../rev1/config.h"
-#endif
-#ifdef SUBPROJECT_rev2
-    #include "../../rev2/config.h"
-#endif
+#include "../../rev2/config.h"
