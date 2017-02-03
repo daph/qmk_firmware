2bc3ffa9a03d61fd31775f29253b69b93d5988e8
David Phillips <david@daviddouglasphillips.com>
2017-02-02 23:53:50 -0800

    LS: swap numpad, add small symbol layer

--- keyboards/lets_split/keymaps/daph/keymap.c
+++ keyboards/lets_split/keymaps/daph/keymap.c
@@ -10,6 +10,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _ARROW 1
+#define _SYMB 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -21,31 +22,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | LGui |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   =  |   ,  |   .  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ~L1  |   \  | Alt  | Ctrl |Space |Enter | Bksp |Shift |  Alt | Down |  Up  |Right |
+ * | ~L1  |   \  | Alt  | Ctrl |Space |Enter | Bksp |Shift |  Alt | Down |  Up  | ~L2  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, \
-  MO(1),   KC_BSLS, KC_LALT, KC_LCTL, KC_SPC,  KC_ENT,  KC_BSPC, KC_LSFT, KC_LALT, KC_DOWN, KC_UP,   KC_RGHT \
+  MO(1),   KC_BSLS, KC_LALT, KC_LCTL, KC_SPC,  KC_ENT,  KC_BSPC, KC_LSFT, KC_LALT, KC_DOWN, KC_UP,   MO(2) \
 ),
 
 /* Arrow
  * ,-----------------------------------------------------------------------------------.
- * |   F1 |  F2  |  F3  |  F4  |  F5  |  F6  |   [  |   1  |   2  |   3  |   =  |RESET |
+ * |   F1 |  F2  |  F3  |  F4  |  F5  |  F6  |   [  |   7  |   8  |   9  |   =  |RESET |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  F7  |  F8  |  UP  |  F9  |  F10 |  F11 |   ]  |   4  |   5  |   6  |   -  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | LEFT | DOWN |RIGHT |      |  F12 |   0  |   7  |   8  |   9  |      |      |
+ * |      | LEFT | DOWN |RIGHT |      |  F12 |      |   1  |   2  |   3  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ARROW] = KEYMAP( \
-  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_LBRC, KC_1,    KC_2,    KC_3,    KC_EQL,  RESET, \
+  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_LBRC, KC_7,    KC_8,    KC_9,    KC_EQL,  RESET, \
   KC_F7,    KC_F8,    KC_UP,    KC_F9,   KC_F10,  KC_F11,  KC_RBRC, KC_4,    KC_4,    KC_6,    KC_MINS, KC_TRNS, \
-  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_TRNS, KC_F12,  KC_0,    KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, \
+  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_TRNS, KC_F12,  KC_TRNS, KC_1,    KC_2,    KC_3,    KC_0,    KC_TRNS, \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+),
+
+/* Arrow
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |  (   |   )  |   [  |   ]  |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_SYMB] = KEYMAP( \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS,  S(KC_9),  S(KC_0),  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
 ),
 };
