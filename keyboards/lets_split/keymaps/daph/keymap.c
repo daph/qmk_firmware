262548db9f250d6952a2bbe5917c89dc64828e6d
David Phillips <david@daviddouglasphillips.com>
2017-02-06 11:44:32 -0800

    LS: Fix 4 key

--- keyboards/lets_split/keymaps/daph/keymap.c
+++ keyboards/lets_split/keymaps/daph/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SYMB] = KEYMAP( \
-  KC_1,     KC_2,     KC_3,     KC_4,    KC_4,    KC_TRNS, KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0, \
+  KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_TRNS, KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0, \
   KC_TRNS,  S(KC_9),  S(KC_0),  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
