2b8bfe12b122bc7872b8697562a63a3e369a980f
David Phillips <david@daviddouglasphillips.com>
2017-02-06 12:33:19 -0800

    LS: Put backtick somewhere

--- keyboards/lets_split/keymaps/daph/keymap.c
+++ keyboards/lets_split/keymaps/daph/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  1   |  2   |   3  |  4   |  5   |      |      |  6   |  7   |  8   |  9   | 0    |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  (   |   )  |   [  |   ]  |      |      |      |      |      |      |      |
+ * |      |  (   |   )  |   [  |   ]  |  `  |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMB] = KEYMAP( \
   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_TRNS, KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0, \
-  KC_TRNS,  S(KC_9),  S(KC_0),  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS,  S(KC_9),  S(KC_0),  KC_LBRC, KC_RBRC, KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
 ),
